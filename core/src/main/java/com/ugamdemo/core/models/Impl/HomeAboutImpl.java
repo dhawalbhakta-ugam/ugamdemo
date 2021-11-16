9f7d394da53fc9f5e009d285afab63cacc5d2bfe
dhawalbhakta-ugam <dhawal.bhakta@ugamsolutions.com>
2021-11-17 00:41:26 +0530

    Update HomeAboutImpl.java

--- core/src/main/java/com/ugamdemo/core/models/Impl/HomeAboutImpl.java
+++ core/src/main/java/com/ugamdemo/core/models/Impl/HomeAboutImpl.java
@@ -1,17 +1,26 @@
 package com.ugamdemo.core.models.Impl;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import com.ugamdemo.core.models.HomeAbout;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.ExporterOption;
 import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 
 @Model(adaptables = Resource.class,
         adapters = HomeAbout.class,
+        resourceType = HomeAboutImpl.RESOURCE_TYPE,
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = "jackson", extensions ="json",selector = "homeabout",
+        options = {
+                @ExporterOption(name = "SerializationFeature.WRAP_ROOT_VALUE", value="true"),
+        })
+@JsonRootName("HomeAbout")
 public class HomeAboutImpl implements HomeAbout{
-
+    final protected static String RESOURCE_TYPE="ugamdemo/components/content/home-about";
     @Inject
     String title;
 
