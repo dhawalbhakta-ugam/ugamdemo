53c190ef25c17580a8bbd9f4ed603a25c80ae482
dhawalbhakta-ugam <dhawal.bhakta@ugamsolutions.com>
2021-12-14 15:16:18 +0530

    recreate task
    all req files are recreated

---
+++ core/src/main/java/com/ugamdemo/core/models/OsgiConfig.java
@@ -0,0 +1,9 @@
+package com.ugamdemo.core.models;
+
+public interface OsgiConfig {
+    public String getServiceName();
+    public int getServiceCount();
+    public boolean isLiveData();
+    public String[] getCountries() ;
+    public String getRunModes();
+}
---
+++ core/src/main/java/com/ugamdemo/core/models/OsgiDemo.java
@@ -0,0 +1,9 @@
+package com.ugamdemo.core.models;
+
+public interface OsgiDemo {
+    public String getServiceName();
+    public int getServiceCount();
+    public boolean isLiveData();
+    public String[] getCountries() ;
+    public String getRunModes();
+}
